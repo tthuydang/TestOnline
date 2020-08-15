b8f42326ce97096357c16d2e891f5a48ef565128
tthuydang <tt.huy.dang@tomosia.com>
2020-08-15 20:22:34 +0700

    reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,2 @@
 class UsersController < ApplicationController
-  CREATOR = 'CREATOR'
-  PARTICIPANT = 'PARTICIPANT'
-
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.save
-
-    redirect_to @user
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:full_name, :email, :password_digest)
-  end
 end
