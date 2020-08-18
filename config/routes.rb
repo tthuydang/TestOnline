61d22ad2d1073f3d04853f49c40b11099b11d0e1
tthuydang <tt.huy.dang@tomosia.com>
2020-08-18 11:45:40 +0700

    remove users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   resources :tickets
   root 'tickets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
