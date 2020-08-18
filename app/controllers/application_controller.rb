240eba1bbea285ea42450c13eacef83d7194695c
tthuydang <tt.huy.dang@tomosia.com>
2020-08-18 11:36:03 +0700

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def index
-    @categories = Category.all
-  end
-
 end
--- app/controllers/sub_tickets_controller.rb
+++
@@ -1,2 +0,0 @@
-class SubTicketsController < ApplicationController
-end
