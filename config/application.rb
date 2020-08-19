b1d8db11a8d48c4ff1c0f3469cd9753c7e0d1110
tung <nguyensontung18183>
2020-08-19 10:10:53 +0700

    add

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +10,15 @@ module TestOnline
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    ActionMailer::Base.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :domain => "mail.google.com",
+      :port => 587,
+      :users_name => "nguyensontung183@gmail.com",
+      :password => "123123",
+      :authentication => "login",
+      :enable_starttls_auto => true,
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controller: { registrations: "registrations" }
   resources :tickets
   root "tickets#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
