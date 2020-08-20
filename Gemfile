2f0beb60e40269810c241bc26ed6717d6ab3a027
tung <nguyensontung18183>
2020-08-20 13:53:03 +0700

    upload avatar

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem "bcrypt", "~> 3.1.7"
 gem "bootsnap", ">= 1.4.2", require: false
 gem "carrierwave"
 gem "letter_opener_web"
-
+gem "mini_magick"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :email, :password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :email, :password, :current_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :email, :password, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :email, :password, :current_password, :avatar])
   end
-
 end
