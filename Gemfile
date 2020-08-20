fbd0ed6d04c0cb37d583e9a09e7d1c30c5533c5b
tung <nguyensontung18183>
2020-08-20 10:46:59 +0700

    validates email name

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem "bcrypt", "~> 3.1.7"
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", ">= 1.4.2", require: false
-
+gem "carrierwave"
 gem "letter_opener_web"
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  VALID_NAME_REGEX = /\A[^0-50`!@#\$%\^&*+_=]+\z/
+  validates :firstname, :lastname, presence: true, format: { with: VALID_NAME_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
