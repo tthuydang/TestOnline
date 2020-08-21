b4e3ca46daac065344dc71fe9ccdace22ce5757f
tthuydang <tt.huy.dang@tomosia.com>
2020-08-21 15:06:42 +0700

    fix UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,6 @@ class CategoriesController < ApplicationController
   before_action :check_creator
 
   def check_creator
-    # neu da dang nhap va co type la CREATOR thi cho pass
     if current_user == nil || current_user.role != "CREATOR"
       redirect_to root_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controller: { registrations: "registrations" }
   resources :tickets
   resources :categories
-  root "tickets#index"
+  root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
