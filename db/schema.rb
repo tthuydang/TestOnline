5b41902c21f26e6166776cce239baf61fd6baf97
tthuydang <tt.huy.dang@tomosia.com>
2020-08-21 12:19:13 +0700

    rename column in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_132314) do
+ActiveRecord::Schema.define(version: 2020_08_21_033715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_132314) do
     t.string "firstname"
     t.string "lastname"
     t.string "avatar"
-    t.string "type"
+    t.string "role"
     t.integer "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
