0b5571d365a7b969ac34d38ed9a1418315f43af4
tthuydang <tt.huy.dang@tomosia.com>
2020-08-27 22:12:28 +0700

    import question & answer

---
+++ app/controllers/creator/questions_controller.rb
@@ -0,0 +1,89 @@
+class Creator::QuestionsController < ApplicationController
+
+  def index
+    redirect_to tickets_path if params[:ticket_code] == nil
+    @ticket = Ticket.where(code: params[:ticket_code]).first
+    @questions = Question.where(ticket_id: @ticket.id)
+  end
+
+  def importfile # custom action
+    file_path = question_file_param[:file_question].path
+    questions = file_path != nil ? get_questions_from_file_text(file_path) : nil
+
+    # xóa hết question cũ, rồi save_to_database
+    Question.where(:ticket_id => question_file_param[:ticket_id]).destroy_all
+    save_to_database(questions, question_file_param[:ticket_id])
+
+    redirect_back(fallback_location: questions_path)
+  end
+
+  private
+
+  def question_file_param
+    params.require(:question).permit(:file_question, :ticket_id)
+  end
+
+  def save_to_database(questions, ticket_id)
+    questions.each_with_index do |quest, index|
+      curr_question = Question.new
+      curr_question.question = quest.question
+      curr_question.ticket_id = ticket_id
+      curr_question.save
+
+      quest.dsAnswers.each_with_index do |ans, i|
+        curr_answer = Answer.new
+        curr_answer.answer = ans.answer
+        curr_answer.is_correct = ans.is_correct
+        curr_answer.question_id = curr_question.id
+        curr_answer.save
+      end
+    end
+  end
+
+  # lấy danh sách question & answer
+  def get_questions_from_file_text(file_path)
+    file = File.new(file_path, 'r')
+    questions = []
+    is_question = true
+    i = 0
+    while line = file.gets
+      if line.strip.length > 0
+        quest = line.match(/[Q]\s\d{1,999}[\.\:\/\)]/)    # Q 94.:/)
+        ans = line.match(/^\w[\.\:\/\)]/)  # a.:/) hoặc A.:/) hoặc 1.:/)
+
+        if quest != nil  # question
+          curr_question = Question.new
+          curr_question.question = line[(quest.to_s.length)..(-1)].strip
+          curr_question.dsAnswers = []
+
+          questions << curr_question
+          i += 1
+          is_question = true
+        elsif ans != nil  # answer
+          s_correct = line.match(/!!!T$/)    # !!!T
+          ans_content = s_correct == nil ? line[(ans.to_s.length)..(-1)].strip : line[(ans.to_s.length)..(-1)].delete("!!!T").strip
+
+          curr_answer = Answer.new
+          curr_answer.answer = ans_content
+          curr_answer.is_correct = s_correct == nil ? false : true
+          questions[i - 1].dsAnswers << curr_answer
+
+          is_question = false
+        else  # dòng xuống hàng của question hoặc answer
+          if is_question  # nối vào question
+            questions[i - 1].question = questions[i - 1].question + " #{line.strip}"
+          else  # nối vào answer
+            # nếu xuống hàng có !!!T
+            s_correct = line.match(/!!!T$/)    # !!!T
+            ans_content = s_correct == nil ? line.strip : line.delete("!!!T").strip
+
+            # cập nhật lại is_correct và answer
+            questions[i - 1].dsAnswers.last.is_correct = true if s_correct != nil
+            questions[i - 1].dsAnswers.last.answer = questions[i - 1].dsAnswers.last.answer + " #{ans_content}"
+          end
+        end
+      end
+    end
+    questions
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   scope module: 'creator' do
     resources :tickets
     resources :categories
+    resources :questions
+    post 'importfile' => "/creator/questions#importfile"
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
