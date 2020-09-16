b75473dbcdf2a79b66d918c09df0e01ed93d0219
tthuydang <tt.huy.dang@tomosia.com>
2020-09-16 09:53:32 +0700

    check if creator

--- app/controllers/creator/histories_controller.rb
+++ app/controllers/creator/histories_controller.rb
@@ -1,6 +1,7 @@
 class Creator::HistoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :shared_pages
+  before_action :check_if_creator
 
   def history
     ticket = Ticket.find_by(code: params[:code])
