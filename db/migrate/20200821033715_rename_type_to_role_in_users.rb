bee17c811d45bde0ee14265c3580d08a1ff7b7a5
tt.huy.dang <tt.huy.dang@tomosia.com>
2020-09-21 11:34:27 +0700

    remove file change type to role

--- db/migrate/20200821033715_rename_type_to_role_in_users.rb
+++ db/migrate/20200821033715_rename_type_to_role_in_users.rb
@@ -1,5 +1,5 @@
-class RenameTypeToRoleInUsers < ActiveRecord::Migration[6.0]
-  def change
-    rename_column "users", "type", "role"
-  end
-end
+# class RenameTypeToRoleInUsers < ActiveRecord::Migration[6.0]
+#   def change
+#     rename_column "users", "type", "role"
+#   end
+# end
