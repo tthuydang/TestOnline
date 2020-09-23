81d7f04f4f38b176b674cf70a987b2edb92b6dec
tt.huy.dang <tt.huy.dang@tomosia.com>
2020-09-23 17:01:29 +0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,11 +116,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "google.com",
+    address: "smtp.gmail.com",
     port: 587,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
+    domain: "quizonline-tomosia.herokuapp.com"
   }
 end
