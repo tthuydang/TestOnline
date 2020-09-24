bdbffaa9c43495c873fa79e26e479a7bffa92ae4
tung <nguyensontung18183>
2020-09-24 09:56:40 +0700

    fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.secret_key_base = ENV["secret_key_base"]
-  config.action_mailer.default_url_options = { host: "https://quizonline-tomosia.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "quizonline-tomosia.herokuapp.com" }
+  # config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -118,9 +119,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    domain: "quizonline-tomosia.herokuapp.com",
   }
 end
